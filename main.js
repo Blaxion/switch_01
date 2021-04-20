1e5ef9ada5169569f0197d7be17e2ca699abe7b5
Blaxion <khibaziev.f@gmail.com>
2021-04-20 22:19:12 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+let x = ((prompt("Le planning de ?")).toUpperCase()).trim();
+
+switch (x) {
+    case "LUNDI":
+        alert("On est lundi mon kiki , MG + foot"); 
+    break;
+
+    case "MARDI":
+        alert("On est mardi fi y a encore MG + resto (dans mes rêves les plus fous)"); 
+    break;
+
+    case "MERCREDI":
+        alert("On est mercredi youpi encore MG + zumba"); 
+    break;
+
+    case "JEUDI":
+        alert("On séche? + On séche?") ;
+    break;
+
+    case "VENDREDI":
+        alert("MG se tenir à carreaux Catus et vénère pcke t'as séché hmar!!!!! " );
+    break;
+
+    case "SAMEDI":
+        alert("Sushi à midi?"); 
+    break;
+
+    case "DIMANCHE":
+        alert("Les restes des sushis, battre Junior au WAKATIME");  
+    break;
+
+    default:
+        alert("CHIEN"); 
+    break;
+}
